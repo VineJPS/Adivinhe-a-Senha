6bd92f24cef880d69cf9f93b95418084054b4af4
VineJPS <vinicius108sousa@gmail.com>
2024-05-12 15:08:04 -0300

    Validando acertos nos lugares certos/acerto total

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+//Pegando dados do formulario
 const n1 = document.getElementById("n1")
 const n2 = document.getElementById("n2");
-const n3 = document.getElementById("n2");
+const n3 = document.getElementById("n3");
 const n4 = document.getElementById("n4");
 
-
+//Gerar senha aleatoria
 function gerarSenhaAleatoria() {
     var senha = '';
     for (var i = 0; i < 4; i++) {
@@ -21,10 +22,34 @@ btn.addEventListener('click', function () {
     let N3 = n3.value;
     let N4 = n4.value;
     
+    //definindo as respostas
     let acerto = false;
     console.log('Senha correta: ' + codigo)
  
-   let numeros_inseridos = [N1, N2, N3, N4];
+    //pegando os numeros
+    let numeros_inseridos = [N1, N2, N3, N4];
+    console.log(numeros_inseridos)
+    
+    //Variavel contadora
+    let count = 0;
+    let acertos = 0;
+    let posicaoErrada = 0;
+
+    //Validadndo os numeros recebidos
+
+    //validando acertos
+    count = 0;
+    acertos = 0;
+    while(count <= 3){
+        if(numeros_inseridos[count] === codigo[count]){
+            acertos = acertos + 1
+        }
+        count = count + 1
+    }
+    console.log('Total de Acertos' + acertos)
+    if(acertos === 4){
+        alert("Acertou tudo!, reinicie a pagina para gerar um novo codigo")
+    }
    
    
    
