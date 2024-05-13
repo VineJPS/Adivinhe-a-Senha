71fd05a1aab4acf7a7a81be52b8fae65b495f63f
VineJPS <vinicius108sousa@gmail.com>
2024-05-13 12:32:45 -0300

    V1 finalizada

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const n4 = document.getElementById("n4");
 
 //Gerar senha aleatoria
 function gerarSenhaAleatoria() {
-    var senha = '';
+    var senha = [];
     for (var i = 0; i < 4; i++) {
         var digitoAleatorio = Math.floor(Math.random() * 10); 
-        senha += digitoAleatorio;
+        senha += [digitoAleatorio];
     }
     return senha;
 }
@@ -29,28 +29,60 @@ btn.addEventListener('click', function () {
     //pegando os numeros
     let numeros_inseridos = [N1, N2, N3, N4];
     console.log(numeros_inseridos)
-    
+
     //Variavel contadora
     let count = 0;
     let acertos = 0;
     let posicaoErrada = 0;
+    let i = 0;
+    let n = 0;
 
     //Validadndo os numeros recebidos
 
-    //validando acertos
-    count = 0;
-    acertos = 0;
+    //validando acertos nos lugares certos
+    // count = 0;
+    // acertos = 0;
+    // while(count <= 3){
+    //     if(numeros_inseridos[count] == codigo[count]){
+    //         acertos = acertos + 1
+    //     }
+    //     count = count + 1
+    // }
+    // console.log('Numeros certos, nos lugares certos => ' + acertos)
+    
+    //Validadndo acetos mas nos lugares errados 
     while(count <= 3){
-        if(numeros_inseridos[count] === codigo[count]){
-            acertos = acertos + 1
+        while(i <= 3 ){
+            if(n == i){
+                if(numeros_inseridos[n] == codigo[i]){
+                    acertos = acertos + 1
+                    console.log (' certo ' + acertos + ' n= ' + n + ' i= ' + i)
+                }
+            }else if(numeros_inseridos[n] == codigo[i]){
+                    posicaoErrada = posicaoErrada + 1
+                    console.log (' errado ' + posicaoErrada + ' n= ' + n + ' i= ' + i)
+            }
+            i++
         }
-        count = count + 1
+        n++
+        if(n > 3){ n = 0 }
+        if(i > 3){ i = 0 }
+        count = count + 1;
     }
-    console.log('Total de Acertos' + acertos)
+
+
+    console.log("Numeros certos mas na posição errada => " + posicaoErrada)
+    console.log("Numeros certos na posição certa => " + acertos)
+
     if(acertos === 4){
         alert("Acertou tudo!, reinicie a pagina para gerar um novo codigo")
+    }else{
+        count = 0;
+        n = 0;
+        i = 0;
+        acertos = 0;
+        posicaoErrada = 0;
     }
-   
-   
+    
    
 })
