78e0bb69e66e964ebcb17cd00adba027ed22e9f0
VineJPS <vinicius108sousa@gmail.com>
2024-05-24 09:59:31 -0300

    Script retornando corretamente os valores pelo innerHTML

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const n2 = document.getElementById("n2");
 const n3 = document.getElementById("n3");
 const n4 = document.getElementById("n4");
 
+const resultados = document.getElementById("results");
 const contado = document.getElementById("contador");
 
+//Variaveis necessárias para colocar no html
 let tentativas = 0;
 //garante que receba somente valores validos
 function validarNumero(input) {
@@ -29,9 +31,9 @@ function gerarSenhaAleatoria() {
     }
     return senha.join('');
 }
-    
-    let codigo = gerarSenhaAleatoria()
-    console.log('Senha correta: ' + codigo)
+
+let codigo = gerarSenhaAleatoria()
+console.log('Senha correta: ' + codigo)
 
 btnEnviar.addEventListener('click', function () {
     //definindo as variaveis
@@ -47,7 +49,6 @@ btnEnviar.addEventListener('click', function () {
 
     //pegando os numeros
     let numeros_inseridos = [N1, N2, N3, N4];
-    console.log(numeros_inseridos)
 
     //Variavel contadora
     let count = 0;
@@ -78,18 +79,37 @@ btnEnviar.addEventListener('click', function () {
     }
 
 
-    alert("Numeros certos mas na posição errada => " + posicaoErrada)
-    alert("Numeros certos na posição certa => " + acertos)
+    //Jogando os valores na tela pelo innerHTML
+    
+        resultados.innerHTML += `
+     <div  class="resposta">
+        <div class="numeracao">
+            <h1>${tentativas}</h3>
+        </div>
+        <div class="numeroInserido coluna">
+            <h3>Numero Inserido</h3>
+            <p>${numeros_inseridos}</p>
+        </div>
+        <div class="lugarCerto coluna">
+            <h3>lugar certo</h3>
+            <p>${acertos}</p>
+        </div>
+        <div class="lugarErrado coluna">
+            <h3>Lugar errado</h3>
+            <p>${posicaoErrada}</p>
+        </div>
+        <div class="certo_errado">
+        ${acertos == 4 ? '<img src="./img/certo.png" alt="" class="imgValiacao">' : '<img src="./img/errado.png" alt="" class="imgValiacao">'}
+        </div>
+        </div>`
 
-    if (acertos === 4) {
-        alert("Acertou tudo!, reinicie a pagina para gerar um novo codigo")
-    } else {
+        if(acertos == 4){
+            alert("Parabens você acertou a senha!, para gerar uma nova clique no botao no canto superior esquerdo")
+        }
         count = 0;
         n = 0;
         i = 0;
         acertos = 0;
         posicaoErrada = 0;
-    }
-
-
+    
 })
